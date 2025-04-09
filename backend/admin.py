c55bfcd2ee7df5940de8f03e97125905d3ad4d4a
Alexey <155527397+ReaGemt@users.noreply.github.com>
2025-04-09 14:58:27 +0300

    Обновление файла backend/admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -4,14 +4,13 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 from starlette.requests import Request
 
 from backend.models import User
-from backend.main import app
+from backend.app_instance import app  # ✅ заменили импорт
 
 class AdminAuth(AdminAuthenticationBackend):
     async def login(self, request: Request) -> bool:
         form = await request.form()
         username = form.get("username")
         password = form.get("password")
-        # Простейшая авторизация (замени на свою)
         return username == "admin" and password == "admin"
 
     async def logout(self, request: Request) -> bool:
@@ -23,7 +22,7 @@ class UserAdmin(ModelView, model=User):
 
 
 admin = Admin(
-    app=app,  # ✅ передаём объект FastAPI
+    app=app,
     authentication_backend=AdminAuth(secret_key="super-secret-key")
 )
 
