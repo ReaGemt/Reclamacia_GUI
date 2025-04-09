74dd2d8e3378f64d469854dbc38de9597d56b946
Alexey <155527397+ReaGemt@users.noreply.github.com>
2025-04-09 16:21:04 +0300

    Разделение Record на ORM-модель и Pydantic-модель

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,27 @@
-# backend/models.py
-
 from sqlalchemy import Column, Integer, String
 from backend.database import Base
 from pydantic import BaseModel
 
+
+# SQLAlchemy ORM-модель
+class RecordORM(Base):
+    __tablename__ = "records"
+
+    id = Column(Integer, primary_key=True)
+    record_date = Column(String)
+    last_name = Column(String)
+    first_name = Column(String)
+    patronymic = Column(String)
+    status = Column(String)
+    comment = Column(String)
+    card_number = Column(String)
+    organization = Column(String)
+    manufacturer = Column(String)
+    work_status = Column(String)
+    created_by = Column(String)
+
+
+# Pydantic модель
 class Record(BaseModel):
     id: int | None = None
     record_date: str
@@ -18,13 +36,16 @@ class Record(BaseModel):
     work_status: str
     created_by: str
 
+
 class LoginRequest(BaseModel):
     login: str
     password: str
 
+
 class CreateUserRequest(BaseModel):
     login: str
     password: str
+
 
 class User(Base):
     __tablename__ = "users"
